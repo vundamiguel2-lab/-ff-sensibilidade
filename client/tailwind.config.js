ffdcf9137c9fc5ab9b6ce40da393526135e05660
vundamiguel2-lab <vundamiguel2@gmail.com>
2026-08-24 02:03:15 +0100

    Criar configuração do Tailwind CSS

---
+++ client/tailwind.config.js
@@ -0,0 +1,16 @@
+export default {
+  content: [
+    './index.html',
+    './src/**/*.{js,jsx}',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        dark: '#0f172a',
+        darker: '#0a0e27',
+        fire: '#ff6b35',
+      },
+    },
+  },
+  plugins: [],
+}
